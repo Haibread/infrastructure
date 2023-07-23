c69019c87cbd21df6b736ed96aa463fa31f6f391
Haibread <39999956+Haibread@users.noreply.github.com>
2023-07-23 10:30:23 +0200

    update k8s image (#1)
    * update k8s image
    
    * update actions workflow
    
    * update workflow
    
    * update workflow
    
    * update workflow
    
    * fix workflow env
    
    * fix tf fmt
    
    * fix tf module output
    
    * fix workflow
    
    * update workflow
    
    * add tf backend
    
    * terraform fmt
    
    * update workflow comment
    
    * update workflow

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,9 +1,9 @@
 module "k8s-rancher-cilium" {
   source = "./kubernetes/k8s-rancher-cilium"
 
-  rancher_url  = "https://192.168.0.5:6587"
+  rancher_url             = "https://192.168.0.5:6587"
   vsphere_vcenter_address = "vcenter.homelab.lan"
-  
+  cluster_image_name      = "jammy-server-cloudimg-prepared-3"
   # Declared by GitLab CI/CD secrets
   #rancher_access_key = ""
   #rancher_secret_key = ""
