162f475e4274dd0e106db8c929907a5e8de60159
Haibread <theobarbou@gmail.com>
2023-07-23 11:29:20 +0200

    fix variables

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,6 +3,10 @@ module "k8s-rancher-cilium" {
 
   rancher_url             = "https://192.168.0.5:6587"
   vsphere_vcenter_address = "vcenter.homelab.lan"
+  rancher_access_key      = var.rancher_access_key
+  rancher_secret_key      = var.rancher_secret_key
+  vsphere_user            = var.vsphere_user
+  vsphere_password        = var.vsphere_password
 
   cluster_name                      = "test-cluster"
   cluster_kubernetes_version        = "v1.26.6+rke2r1"
