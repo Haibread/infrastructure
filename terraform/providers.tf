3d0391122c5e3b9378cccff950e6fce26d79b9c7
Haibread <theobarbou@gmail.com>
2023-07-23 10:22:10 +0200

    add tf backend

---
+++ terraform/providers.tf
@@ -0,0 +1,10 @@
+terraform {
+    backend "s3" {
+      bucket = "homelab-s3-terraforming"
+      key    = "terraform.tfstate"
+      region = "gra"
+      endpoint = "s3.gra.perf.cloud.ovh.net"
+      skip_credentials_validation = true
+      skip_region_validation = true
+    }
+}
