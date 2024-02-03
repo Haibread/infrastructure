b72f6f8c6a48227e0a38c5bf582cf7ad70a84514
Haibread <theobarbou@gmail.com>
2024-02-03 16:45:47 +0100

    add ssh key to machine

--- infrastructure-terraform/virtual_machines.tf
+++ infrastructure-terraform/virtual_machines.tf
@@ -54,4 +54,9 @@ resource "vsphere_virtual_machine" "test-machine" {
       ipv4_gateway = "10.0.10.254"
     }
   }
+  vapp {
+    properties = {
+      "guestinfo.public-keys" = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC9s7RxUz0DkPWRBA6FQjPWdeTy1sC/1LAM4PxHVJ241A7dgvBRIwEcKcLwVs8INPEOc5qxJNJW8OiCy+W/tsLt7MY1NmCaEiN0MZ+4rTnABBQfMubMb3i2SzHt9k/gBvSZT07kIxD2AoTbITHzK2BAdT7KG1HMFM6tKz92KNCriFeMo2SE8tU8KK4IhH4k865mcfxaJRLaikZtQb+5M3jHGsolWg8mjOMgwGpU6NT0rkxYmInShIcZrvLdoAZRMMiNYWDZYiOu51h9VbDIjg14Tku3MTq5Ivl4bsO1Rfjt47AJFsfHQyz759kZSZ4sMoQPNMe7HMc0N4Y7gUUr8oeR haibread"
+    }
+  }
 }
