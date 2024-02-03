c3cf4f4916020d04fe155dc04a013bdf9bb8ce04
Haibread <theobarbou@gmail.com>
2024-02-03 17:04:00 +0100

    use hostname from vapp

--- infrastructure-terraform/virtual_machines.tf
+++ infrastructure-terraform/virtual_machines.tf
@@ -46,10 +46,10 @@ resource "vsphere_virtual_machine" "test-machine" {
   clone {
     template_uuid = data.vsphere_content_library_item.jammy-server.id
     customize {
-      linux_options {
-        host_name = "test-vm"
-        domain    = "homelab.lan"
-      }
+      # linux_options {
+      #   host_name = "test-vm"
+      #   domain    = "homelab.lan"
+      # }
       network_interface {
         ipv4_address = "10.0.10.2"
         ipv4_netmask = "24"
@@ -60,6 +60,7 @@ resource "vsphere_virtual_machine" "test-machine" {
   vapp {
     properties = {
       "public-keys" = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC9s7RxUz0DkPWRBA6FQjPWdeTy1sC/1LAM4PxHVJ241A7dgvBRIwEcKcLwVs8INPEOc5qxJNJW8OiCy+W/tsLt7MY1NmCaEiN0MZ+4rTnABBQfMubMb3i2SzHt9k/gBvSZT07kIxD2AoTbITHzK2BAdT7KG1HMFM6tKz92KNCriFeMo2SE8tU8KK4IhH4k865mcfxaJRLaikZtQb+5M3jHGsolWg8mjOMgwGpU6NT0rkxYmInShIcZrvLdoAZRMMiNYWDZYiOu51h9VbDIjg14Tku3MTq5Ivl4bsO1Rfjt47AJFsfHQyz759kZSZ4sMoQPNMe7HMc0N4Y7gUUr8oeR haibread"
+      "hostname" = "test-vm.homelab.lan"
     }
   }
 }
