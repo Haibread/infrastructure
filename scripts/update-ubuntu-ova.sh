c6e4471e48336848095e7df2644c4d8f348226cf
Haibread <39999956+Haibread@users.noreply.github.com>
2024-02-03 13:00:09 +0100

    add actions workflow (#3)

---
+++ scripts/update-ubuntu-ova.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+TARGET_LIBRARY_NAME=/ubuntu-cloudimg/
+TARGET_VM_NAME="jammy-server-cloudimg-amd64.ova"
+IMAGE_LIST=$(govc library.info -json $TARGET_LIBRARY_NAME)
+
+if echo "$IMAGE_LIST" | jq -r ". | contains([\"$TARGET_VM_NAME\"])"; then
+    govc library.rm $TARGET_LIBRARY_NAME$TARGET_VM_NAME
+else
+    echo "VM $TARGET_VM_NAME not found."
+fi
+
+govc library.import ubuntu-cloudimg https://cloud-images.ubuntu.com/jammy/current/jammy-server-cloudimg-amd64.ova
